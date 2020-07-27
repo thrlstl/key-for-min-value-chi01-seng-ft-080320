4822b16b0e929506dacfa053a0981838429db8d6
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 20:13:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,3 +5,18 @@ def key_for_min_value(name_hash)
  hash.each do |key, value|
    while 
 end
+
+def key_for_min_value(name_hash)
+  if name_hash == {}
+    return
+  end
+  smallest = nil
+  lowest_item = nil 
+  name_hash.each do |name, num|
+    if smallest == nil || num < smallest
+      smallest = num
+      lowest_item = name
+    end
+  end
+  return lowest_item
+end
